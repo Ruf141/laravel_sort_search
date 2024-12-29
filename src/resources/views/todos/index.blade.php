99d24fcf27f6c74e6d8aa231e9718bbfd2084e17
Ruf141 <mad0908141@gmail.com>
2024-12-29 16:42:31 +0900

    fix

--- src/resources/views/todos/index.blade.php
+++ src/resources/views/todos/index.blade.php
@@ -29,8 +29,8 @@
             <div class="card-body">
                 <div class="row">
                     <div class="form-group col-sm-6">
-                        {!! Form::label('queryText', 'Title')!!}
-                        {!! Form::text('queryText', '', ['class'=>'form-control','placeholder'=> 'キーワードを入力'])!!}
+                        {!! Form::label('queryText', 'Title')!! }
+                        {!! Form::text('queryText', '', ['class'=>'form-control','placeholder'=> 'キーワードを入力'])!! }
                     </div>
                     <div class="form-group col-sm-4">
                         {!! Form::label('status', 'Status')!!}
